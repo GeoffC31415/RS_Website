a4cb3462bed1af9166e7ccd7ea32a75f90e30f9f
GeoffC31415 <geoff.cunningham@organicdrive.co.uk>
2017-10-25 10:25:09 +0100

    Created data table of sensor readings

--- php/RS_tablerows.php
+++ php/RS_tablerows.php
@@ -1,7 +1,5 @@
 <?php 
 
-echo date('Y-m-d H:i:s');
-
 // CONNECT
 
 	// Set database login details
@@ -21,16 +19,24 @@ echo date('Y-m-d H:i:s');
     }
 	
 	
-// QUERY
+// QUERY AND POPULATE ARRAYS
 	
 	// Get all the connected sensors from the database and place in an array
-    $sql = "SELECT SensorName FROM Sensors";
+    $sql = 	"SELECT SensorReadings.LogTime AS DT, Sensors.SensorName AS SenName, SensorReadings.Reading AS Reading
+				FROM SensorReadings INNER JOIN Sensors ON SensorReadings.SensorID = Sensors.ID
+			";
+			
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
+		$times = array();
         $sensornames = array();
+		$readings = array();
+		
         while($row = mysqli_fetch_assoc($result)) {
-            array_push($sensornames, $row['SensorName']);
+			array_push($times, $row['DT']);
+			array_push($sensornames, $row['SenName']);
+			array_push($readings, $row['Reading']);
         }
     }
 	
@@ -38,15 +44,16 @@ echo date('Y-m-d H:i:s');
     mysqli_close($conn);
 	
 	
-// DISPLAY
+// DISPLAY TABLE ROWS
 	
-	$sensorcount = count($sensornames);
-
-	for($x = 0; $x < $sensorcount; $x++) {
-		echo $sensornames[$x];
-		echo "<br>";
+	$readingnum = count($readings);
+	
+	for($x = 0; $x < $readingnum; $x++) {
+		echo "<tr>"
+		echo "<td>$times[$x]</td>"
+		echo "<td>$sensornames[$x]</td>"
+		echo "<td>$readings[$x]</td>"
+		echo "</tr>"
 	}
-
-	phpinfo(); 
 
 ?>
