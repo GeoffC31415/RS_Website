37eebb78e9cbc4febd1dd1a890a644bf3eeea1d2
= <=>
2017-10-25 18:07:52 +0100

    Better tables, pH logging

--- RS_sensortable.php
+++ RS_sensortable.php
@@ -1,36 +1,22 @@
 <!DOCTYPE html>
+<head>
+	<link rel="stylesheet" type="text/css" href="RS_Website/mystyle.css">
+</head>
+
 <html>
 	<body>
+		<?php include "RS_Website/php/RS_db_handler.php"; ?>
 
-		<table style="width:100%">
-		  <tr>
-			<th>Date Time</th>
-			<th>Sensor</th> 
-			<th>Reading</th>
-		  </tr>
+		<h1> Current Readings </h1>
+		<?php 
+			getTopReadings(3);
+		?>
+		
+		<h1> Historical Readings </h1>
 
-		  <?php 
-
-			// QUERY AND POPULATE ARRAYS
-				
-				include "RS_Website/php/RS_db_handler.php";
-				
-			// DISPLAY TABLE ROWS
-				
-				$htmlstr = "";
-				$readingnum = count($data);
-				
-				for($x = 0; $x < $readingnum; $x++) {
-					$htmlstr .= "<tr>";
-					$htmlstr .= "<td>$times[$x]</td>";
-					$htmlstr .= "<td>$sensornames[$x]</td>";
-					$htmlstr .= "<td>$data[$x]</td>";
-					$htmlstr .= "</tr>";
-				}
-				
-				echo $htmlstr;
-				
-		  ?>
+		<?php
+			getSensorData(3);
+		?>
 		  
 		</table>
 
--- php/RS_db_handler.php
+++ php/RS_db_handler.php
@@ -1,28 +1,72 @@
 <?php
-	
+
+function getTopReadings($sensorcount) {
+					
 	// Ensure connection is set up
 	require "RS_db_connect.php";
+
+	$sql = 	"SELECT SensorReadings.LogTime AS DateTime, Sensors.SensorName AS Sensor, SensorReadings.Reading AS Reading
+						FROM SensorReadings INNER JOIN Sensors ON SensorReadings.SensorID = Sensors.ID
+						ORDER BY SensorReadings.LogTime DESC LIMIT $sensorcount;
+					";
 	
-	// Get all the connected sensors from the database and place in an array
-    $sql = 	"SELECT SensorReadings.LogTime AS DT, Sensors.SensorName AS SenName, SensorReadings.Reading AS Reading
-							FROM SensorReadings INNER JOIN Sensors ON SensorReadings.SensorID = Sensors.ID
-						";
-						
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        $times = array();
-        $sensornames = array();
-        $data = array();
-        
-        while($row = mysqli_fetch_assoc($result)) {
-            array_push($times, $row['DT']);
-			array_push($sensornames, $row['SenName']);
-			array_push($data, $row['Reading']);
-        }
+	$result = mysqli_query($conn, $sql);
+	echo_result_as_table($result);
+	if ($result) mysqli_free_result($result);
+	mysqli_close($conn);
+	
+	return 0;
+}
+	
+function getSensorData($num_sensors) {
+
+	// Ensure connection is set up
+	require "RS_db_connect.php";
+
+	for ($x=1; $x < $num_sensors+1; $x++) {
+
+		// Get all the connected sensors from the database and place in an array
+		$sql = 	"SELECT SensorReadings.LogTime AS DateTime, Sensors.SensorName AS Sensor, SensorReadings.Reading AS Reading
+								FROM SensorReadings INNER JOIN Sensors ON SensorReadings.SensorID = Sensors.ID
+								WHERE SensorID = $x
+								ORDER BY SensorReadings.LogTime DESC LIMIT 100;
+				";
+							
+		$result = mysqli_query($conn, $sql);
+		echo_result_as_table($result);
+		echo "\n\n";
+		if ($result) mysqli_free_result($result);
+		
+	}
+	
+	mysqli_close($conn);
+	
+	return 0;
+}
+
+function table_cell($item, $header=false) {
+    if (!$item) return '';
+    $elemname = ($header) ? 'th' : 'td';
+    $escitem = htmlspecialchars($item, ENT_NOQUOTES, 'UTF-8');
+    return "<{$elemname}>{$escitem}</{$elemname}>";
+}
+function table_header_cell($item) {
+    return table_cell($item, true);
+}
+function table_row($items, $header=false) {
+    $func = ($header) ? 'table_header_cell' : 'table_cell';
+    return "<tr>\n\t".implode("\n\t", array_map($func, $items))."\n</tr>\n";
+}
+
+function echo_result_as_table($result) {
+    if ($result && $row = mysqli_fetch_assoc($result)) {
+        $columnnames = array_keys($row);
+        echo "<table>\n", table_row($columnnames, true), "\n";
+        do {
+            echo table_row($row), "\n";
+        } while ($row = mysqli_fetch_assoc($result));
+        echo "</table>\n";
     }
-	
-    mysqli_free_result($result);
-    mysqli_close($conn);
-	
+}
+
 ?>
