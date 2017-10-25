5df1f1a94ba37781a0f888205182646f029ee7c5
GeoffC31415 <geoff.cunningham@organicdrive.co.uk>
2017-10-25 09:59:27 +0100

    Merged files

--- index.php
+++ index.php
@@ -2,15 +2,51 @@
 
 echo date('Y-m-d H:i:s');
 
-include "php/RS_db_handler.php"
+// CONNECT
 
-$sensorcount = count($sensornames);
+	// Set database login details
+    $servername = "localhost";
+    $username = "root";
+    $password = "RoquetteScience";
+    $dbname = "RS_Logs";
 
-for($x = 0; $x < $sensorcount; $x++) {
-	echo $sensornames[$x];
-	echo "<br>";
-}
+    // Create connection
 
-phpinfo(); 
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+	
+	
+// QUERY
+	
+	// Get all the connected sensors from the database and place in an array
+    $sql = "SELECT SensorName FROM Sensors";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        $sensornames = array();
+        while($row = mysqli_fetch_assoc($result)) {
+            array_push($sensornames, $row['SensorName']);
+        }
+    }
+	
+    mysqli_free_result($result);
+    mysqli_close($conn);
+	
+	
+// DISPLAY
+	
+	$sensorcount = count($sensornames);
+
+	for($x = 0; $x < $sensorcount; $x++) {
+		echo $sensornames[$x];
+		echo "<br>";
+	}
+
+	phpinfo(); 
 
 ?>
